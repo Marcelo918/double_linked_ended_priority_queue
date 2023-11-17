a55a367b0f9fed7911fc21b36c05e18bdf4041b3
Marcelo918 <111820089+Marcelo918@users.noreply.github.com>
2023-11-16 20:50:17 -0800

    Added the display function to the Link class

--- double_linked_ended_priority_queue.cpp
+++ double_linked_ended_priority_queue.cpp
@@ -1,5 +1,5 @@
-#include <iostream>     //need this for COUT and CIN
-using namespace std;    //need this for COUT and CIN
+#include <iostream>  //need this for COUT and CIN
+using namespace std; // need this for COUT and CIN
 #include <iomanip>
 
 class Link
@@ -7,11 +7,16 @@ class Link
 public:
     int data;
     Link *prev, *next;
-    Link (int newData)
+    Link(int newData)
     {
         prev = NULL;
         next = NULL;
         data = newData;
     }
 
+    void display()
+    {
+        // show memory address of previous and next pointers as well as itself
+        cout << "{ " << setw(3) << data << "\t:\t" << setw(14) << prev << "\t:\t\t" << &data << "\t:\t" << setw(14) << next << " }" << endl;
+    }
 };
